01f3400809bc2269eb77acb7f86d234747709d59
Maruf Bepary <bepary71@gmail.com>
2023-05-16 20:22:09 +0100

    Implemented signing into an existing account - Error shown if the account does not exist

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -8,6 +8,7 @@ import AuthSocialButton from "./AuthSocialButtont";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -46,7 +47,22 @@ const AuthForm: React.FC = () => {
     }
 
     if (variant === "LOGIN") {
-      // todo: NextAuth signin
+      if (variant === "LOGIN") {
+        signIn("credentials", {
+          ...data,
+          redirect: false,
+        })
+          .then((callback) => {
+            if (callback?.error) {
+              toast.error("Invalid credentials!");
+            }
+
+            if (callback?.ok && !callback?.error) {
+              toast.success("Logged In");
+            }
+          })
+          .finally(() => setIsLoading(false));
+      }
     }
   };
 
