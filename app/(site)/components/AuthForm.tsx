20039d22d92e13ea583eb96390b1dfbf2ee0a70b
Maruf Bepary <bepary71@gmail.com>
2023-05-18 21:47:15 +0100

    Added redirect after successful sign in - Created simple users page with a log out button to clear the session (doesn't actually redirect)

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -9,19 +9,21 @@ import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm: React.FC = () => {
   const session = useSession();
+  const router = useRouter();
   const [variant, setVariant] = useState<Variant>("LOGIN");
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (session?.status === "authenticated") {
-      console.log("Authenticated");
+      router.push("/users");
     }
-  }, [session?.status]);
+  }, [session?.status, router]);
 
   const toggleVariant = useCallback(() => {
     if (variant === "LOGIN") {
@@ -49,6 +51,7 @@ const AuthForm: React.FC = () => {
     if (variant === "REGISTER") {
       axios
         .post("/api/register", data)
+        .then(() => signIn("credentials", data)) // automatically log in after registering
         .catch(() => toast.error("Something went wrong"))
         .finally(() => setIsLoading(false));
     }
@@ -66,6 +69,7 @@ const AuthForm: React.FC = () => {
 
             if (callback?.ok && !callback?.error) {
               toast.success("Logged In");
+              router.push("/users");
             }
           })
           .finally(() => setIsLoading(false));
