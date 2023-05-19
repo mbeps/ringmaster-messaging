a2bbb6ba2ca707499f509f71a9945d5a941c974a
Maruf Bepary <bepary71@gmail.com>
2023-05-19 20:07:44 +0100

    User can start conversation when clicking on a contact - If the conversation does not exist, a new one is created - If the conversation exists, it is reused - Creates group chat

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -13,9 +13,21 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
+  const handleClick = useCallback(() => {
+    setIsLoading(true);
+
+    axios
+      .post("/api/conversations", { userId: data.id })
+      .then((data) => {
+        router.push(`/conversations/${data.data.id}`);
+      })
+      .finally(() => setIsLoading(false));
+  }, [data, router]);
+
   return (
     <>
       <div
+        onClick={handleClick}
         className="
           w-full 
           relative 
