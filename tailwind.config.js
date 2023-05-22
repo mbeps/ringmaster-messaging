8ed632ac473ff91a6d4fb0a818111711f7decf3d
Maruf Bepary <bepary71@gmail.com>
2023-05-22 12:35:59 +0100

    Documented Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+   // Specifies the files that should be scanned by Tailwind CSS for CSS class extraction. 
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  // Allows you to extend or customize the default Tailwind CSS theme
   theme: {
     extend: {
       
     },
   },
+  // Allows you to add plugins to Tailwind CSS
   plugins: [
     require('@tailwindcss/forms')({
       strategy: 'class'
