3a5cc39efe7be1e3cda7bd670c7060b9f79fb3f0
Maruf Bepary <bepary71@gmail.com>
2023-05-22 16:16:27 +0100

    Documented and commented hook for managing active lists for dealing with adding and removing from Pusher channels

--- app/hooks/useActiveList.ts
+++ app/hooks/useActiveList.ts
@@ -1,12 +1,17 @@
 import { create } from "zustand";
 
 interface ActiveListStore {
-  members: string[];
-  add: (id: string) => void;
-  remove: (id: string) => void;
-  set: (ids: string[]) => void;
+  members: string[]; // list of active members
+  add: (id: string) => void; // function to add a member to the list
+  remove: (id: string) => void; // function to remove a member from the list
+  set: (ids: string[]) => void; // function to set the list of active members
 }
 
+/**
+ * Creates a Zustand store using the create function.
+ * The store manages a list of active members and provides functions to add, remove, and set the members.
+ * Returns the store, allowing components to access and update the state of the active members list.
+ */
 const useActiveList = create<ActiveListStore>((set) => ({
   members: [],
   add: (id) => set((state) => ({ members: [...state.members, id] })),
