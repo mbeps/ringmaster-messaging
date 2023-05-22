46a99a94638a6095ed9ee5ceab360fb01b87e49a
Maruf Bepary <bepary71@gmail.com>
2023-05-22 21:46:05 +0100

    Added password check when registering  - at least 8 characters long  - at least 1 number  - at least 1 special character  - at least 1 capital letter  - at least 1 lower case letter

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,6 +4,36 @@ import prisma from "@/app/libs/prismadb";
 import { NextResponse } from "next/server";
 
 /**
+ * Checks if the password meets the following requirements:
+ * - at least 8 characters long
+ * - at least 1 number
+ * - at least 1 special character
+ * - at least 1 capital letter
+ * - at least 1 lower case letter
+ *
+ * If the password does not meet the requirements, an error is thrown.
+ *
+ * @param password (string): password to be checked
+ */
+async function checkPassword(password: string) {
+  if (password.length < 8) {
+    throw new Error("Password must be at least 8 characters long");
+  }
+  if (!/\d/.test(password)) {
+    throw new Error("Password must contain at least 1 number");
+  }
+  if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+    throw new Error("Password must contain at least 1 special character");
+  }
+  if (!/[A-Z]/.test(password)) {
+    throw new Error("Password must contain at least 1 capital letter");
+  }
+  if (!/[a-z]/.test(password)) {
+    throw new Error("Password must contain at least 1 lower case letter");
+  }
+}
+
+/**
  * A post request route for registering a new user.
  *
  * @param request (Request): email, name, password for registration
@@ -15,6 +45,10 @@ export async function POST(request: Request) {
     const body = await request.json();
     // destructuring the body to get the email, name, and password
     const { email, name, password } = body;
+
+    // Password check
+    await checkPassword(password);
+
     // hashes the password with 12 rounds of salting
     const hashedPassword = await bcrypt.hash(password, 12);
 
@@ -31,6 +65,6 @@ export async function POST(request: Request) {
     return NextResponse.json(user);
   } catch (error: any) {
     console.log("ERROR: Registration Unsuccessful", error);
-    return new NextResponse("Internal Error", { status: 500 });
+    return new NextResponse(error.message, { status: 500 });
   }
 }
