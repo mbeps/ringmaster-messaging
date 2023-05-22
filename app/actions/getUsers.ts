1a08caa1ace424884d658d7530742f0dd07e7ee9
Maruf Bepary <bepary71@gmail.com>
2023-05-22 14:54:02 +0100

    Added documentation and comments for the action that retrieves all the users in the platform

--- app/actions/getUsers.ts
+++ app/actions/getUsers.ts
@@ -1,14 +1,24 @@
 import prisma from "@/app/libs/prismadb";
 import getSession from "./getSession";
 
+/**
+ * Gets a list of all the users that exist in the platform.
+ * This excludes the current user who is logged in as the current user cannot message themselves.
+ * User must be logged in to get the list of users.
+ *
+ * @returns (User[]) - Returns a list of users that exist in the platform
+ */
 const getUsers = async () => {
+  // gets the user session for the current user who is logged in
   const session = await getSession();
 
+  // if the user is not logged in, return an empty array
   if (!session?.user?.email) {
     return [];
   }
 
   try {
+    // find the users in the database excluding the current user
     const users = await prisma.user.findMany({
       orderBy: {
         createdAt: "desc",
