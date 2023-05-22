3b4342a71cbcee5a86e51e85279eba683e4ea0b5
Maruf Bepary <bepary71@gmail.com>
2023-05-22 15:00:29 +0100

    Specified that this is a client side component

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -9,6 +9,7 @@ export interface AuthContextProps {
 /**
  * Provides the authentication context for the application.
  * All the children will have access to the authentication context.
+ * This is a client side component.
  *
  * @param param0 {children: React.ReactNode}: children which will have access to the authentication context.
  * @returns (React.ReactNode): children with access to the authentication context.
