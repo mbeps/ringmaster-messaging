5973fa7d6a7d3b052df2dd9e1812421f7116725f
Maruf Bepary <bepary71@gmail.com>
2023-05-22 14:42:44 +0100

    Added documentation and comments for the action for retrieving messages for a given conversation

--- app/actions/getMessages.ts
+++ app/actions/getMessages.ts
@@ -1,7 +1,14 @@
 import prisma from "@/app/libs/prismadb";
 
+/**
+ * Gets a list of messages for a given conversation.
+ *
+ * @param conversationId (string): conversation for which the messages are being retrieved
+ * @returns ((Message & {seen: User[];sender: User;})[]): list of messages and users
+ */
 const getMessages = async (conversationId: string) => {
   try {
+    // find the messages in the database for the given conversation
     const messages = await prisma.message.findMany({
       where: {
         conversationId: conversationId,
