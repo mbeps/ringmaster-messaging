bedf317fdce4d57d81d5ee94fc1a6894b82aef0b
Maruf Bepary <bepary71@gmail.com>
2023-05-22 14:46:35 +0100

    Added documentation for the action to retrieve the session and its information

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -2,6 +2,12 @@ import { getServerSession } from "next-auth";
 
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
+/**
+ * Get the current session from the server.
+ * Uses `getServerSession` from `next-auth` to get the session from the previously defined `authOptions`.
+ *
+ * @returns (Promise<Session | null>): The current session from the server.
+ */
 export default async function getSession() {
   return await getServerSession(authOptions);
 }
