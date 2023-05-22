db68fe09eb33864414f233193932943e03c7c73e
Maruf Bepary <bepary71@gmail.com>
2023-05-22 14:54:34 +0100

    Added reasoning behind this action

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -5,6 +5,7 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 /**
  * Get the current session from the server.
  * Uses `getServerSession` from `next-auth` to get the session from the previously defined `authOptions`.
+ * This makes it easier to get the session from the server.
  *
  * @returns (Promise<Session | null>): The current session from the server.
  */
